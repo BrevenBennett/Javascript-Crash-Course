819a03cf3e039e502aec5eba22a410f05332aa79
BrevenBennett <Breven.stars@gmail.com>
2022-12-12 14:23:02 -0600

    learned data types and variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// let bool = '1' === 1
+// console.log(bool)
+
+let subscribed = false
+
+if (subscribed === true) {
+    console.log('show the video')
+} 
+else {
+    console.log('tell user to log in')
+}
