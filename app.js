a38d3e7b87c378bb1dd1f39db8d7e72253465dd7
BrevenBennett <Breven.stars@gmail.com>
2022-12-14 09:14:11 -0600

    finished learning core principles of JS

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-// let bool = '1' === 1
-// console.log(bool)
+let users = [
+    {
+        username: 'David',
+        email: 'david@frontendsimplified',
+        password: 'test123',
+        subscriptionStatus: 'VIP',
+        discordId: 'David Bragg#0001',
+        lessonsCompleted: [0, 1]
+    },
+    {
+        username: 'Mitri',
+        email: 'mitri@frontendsimplified',
+        password: 'mitri123',
+        subscriptionStatus: 'VIP',
+        discordId: 'Mitri#0001',
+        lessonsCompleted: [0, 1, 2, 3]
+    },
+    {
+        username: 'Zen',
+        email: 'Zen@frontendsimplified',
+        password: 'zen123',
+        subscriptionStatus: 'VIP',
+        discordId: 'zen#0001',
+        lessonsCompleted: [0, 1]
+    }
+]
 
-let subscribed = false
-
-if (subscribed === true) {
-    console.log('show the video')
-} 
-else {
-    console.log('tell user to log in')
+function logIn (email, password) {
+    for(i = 0; i < users.length; ++i) {
+        if(users[i].email === email) {
+            if(users[i].password === password) {
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('password is incorrect - try again')
+            }
+            return
+        }
+    }
+    console.log('could not find an email that matches')
 }
+
+logIn('doesnotexist@gmail', 'wrong password')
+
+function register(user) {
+  users.push(user)
+}
+
+register({
+  username: "Zen",
+  email: "Zen@frontendsimplified.com",
+  password: "zen123",
+  subscriptionStatus: "VIP",
+  discordId: "zen#0001",
+  lessonsCompleted: [0, 1]
+});
+
+console.log(users)
